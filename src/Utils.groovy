c2f7fcc84e90519f1ecfa70d31103860effe64ea
Farshid Mahdavipour <farshid@dvwarehouse.com>
2019-08-13 13:30:45 -0700

    echo

--- src/Utils.groovy
+++ src/Utils.groovy
@@ -1,11 +1,11 @@
 class Utils {
     static def info(def steps, String message)
     {
-        echo "INFO: ${message}"
+        script.echo ("INFO: $message")
     }
 
     static def warning(def steps, String message) 
     {
-        echo "WARNING: ${message}"
+        script.echo ("WARNING: $message")
     }
 }
