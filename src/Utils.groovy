f1cd6777bd53861bb72096b872c85d6c51e8e964
Farshid Mahdavipour <farshid@dvwarehouse.com>
2019-08-13 13:36:34 -0700

    echo

--- src/Utils.groovy
+++ src/Utils.groovy
@@ -1,11 +1,11 @@
 class Utils {
     static def info(def steps, String message)
     {
-        script.echo("Hello there $message")
+        steps.echo("Hello there $message")
     }
 
     static def warning(def steps, String message) 
     {
-        script.echo("Hello there $message")
+        steps.echo("Hello there $message")
     }
 }
